52d19ef2f883d93466e3d38eaa65d7d7490773c6
Ole Kirchner <ole_kirchner@gmx.de>
2024-10-17 10:54:26 +0200

    Added reading feeds from JSON from newspaperss directory

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
+import os
+import json
 import curses
+
+
+def get_json_data(path):
+    abs_path = os.path.expanduser(path)
+
+    with open(abs_path, encoding="UTF-8") as file:
+        return json.load(file)
 
 
 def main(stdscr):
@@ -9,15 +18,19 @@ def main(stdscr):
     curses.curs_set(0)
 
     stdscr_num_of_lines, stdscr_num_of_cols = stdscr.getmaxyx()
-    num_of_feeds = 100
+
+    feeds = get_json_data("~/.newspaperss/feeds.json")
+    num_of_feeds = len(feeds)
 
     title_win = curses.newwin(1, stdscr_num_of_cols, 0, 0)
     title_win.addstr(0, 0, "Newspaperss v0.1")
     title_win.refresh()
 
     feed_win = curses.newpad(num_of_feeds, stdscr_num_of_cols)
-    for x in range(num_of_feeds):
-        feed_win.addstr(x, 0, f"Feed {x + 1}")
+    for idx, feed in enumerate(feeds):
+        feed_title = feed["title"]
+        feed_win.addstr(idx, 0, str(idx + 1))
+        feed_win.addstr(idx, 5, feed_title)
     selected_feed = 0
     feed_win_offset = 0
     feed_win.chgat(selected_feed, 0, -1, curses.A_REVERSE)
@@ -44,8 +57,10 @@ def main(stdscr):
                     feed_win_offset += 1
 
         feed_win.erase()
-        for x in range(num_of_feeds):
-            feed_win.addstr(x, 0, f"Feed {x + 1}")
+        for idx, feed in enumerate(feeds):
+            feed_title = feed["title"]
+            feed_win.addstr(idx, 0, str(idx + 1))
+            feed_win.addstr(idx, 5, feed_title)
         feed_win.chgat(selected_feed, 0, -1, curses.A_REVERSE)
 
         feed_win.refresh(
